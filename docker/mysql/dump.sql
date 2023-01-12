da7ee4145cff1da7b1a184cf88f5db8c834949ae
amr9mohamed <amrmohamed53@gmail.com>
2023-01-12 22:38:22 +0200

    define initial schema

--- docker/mysql/dump.sql
+++ docker/mysql/dump.sql
@@ -1,4 +1,17 @@
-CREATE TABLE `example` (
-  `id` INT NOT NULL auto_increment,
-  PRIMARY KEY (`id`)
+CREATE TABLE tables
+(
+    id        INT NOT NULL auto_increment,
+    capacity  INT,
+    available INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE guests
+(
+    name         VARCHAR(255) UNICODE,
+    table_id     INT,
+    accompanying INT,
+    time_Arrived DATETIME,
+    PRIMARY KEY (name),
+    FOREIGN KEY (table_id) REFERENCES tables (id)
 );
